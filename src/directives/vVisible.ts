9169f97561488b1707ef5458491da351ea5923ff
Mathiew82 <amateo1982@yahoo.es>
2025-12-30 23:47:19 +0100

    feat!: simplify v-visible directive to accept only a callback function

--- src/directives/vVisible.ts
+++ src/directives/vVisible.ts
@@ -1,19 +1,14 @@
 import type { DirectiveBinding } from 'vue';
 import {
   VisibilityObserver,
-  type VisibilityOptions,
   type VisibilityCallback,
 } from '../core/visibilityObserver';
-
-interface DirectiveValue extends VisibilityOptions {
-  callback: VisibilityCallback;
-}
 
 const observer = new VisibilityObserver();
 
 export const vVisible = {
-  mounted(el: HTMLElement, binding: DirectiveBinding<DirectiveValue>) {
-    observer.observe(el, binding.value.callback, binding.value);
+  mounted(el: HTMLElement, binding: DirectiveBinding<VisibilityCallback>) {
+    observer.observe(el, binding.value, { once: false });
   },
   unmounted(el: HTMLElement) {
     observer.unobserve(el);
